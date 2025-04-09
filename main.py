db5f47ee4d2fcd7403ac55318a129f65f3680e8d
leoscarton <lfscarton@gmail.com>
2025-04-08 21:02:40 -0300

    Added basic habit functions

--- main.py
+++ main.py
@@ -13,8 +13,40 @@ class Habit:
     def __repr__(self):
         return f"Habit Data: Name = {self.name}, Type = {self.type}, Weekly Frequency = {self.week_frequency}, Done? = {'Yes' if self.check else 'No'}"
 
+    def change_name(self, new_name:str):
+        assert new_name is not None and (len(new_name) > 0), "New name must not be empty."
+        assert new_name != self.name, "New name must be different from the current name."
+        
+        self.name = new_name
 
+class HabitGroup():
+    def __init__(self, name:str, habits:list[Habit] = []):
+        self.name = name
+        self.habits = habits
+
+    def __repr__(self):
+        return f"Habit Group: Name = {self.name}, Habits = {self.habits}"
+    
+    def change_group_name(self, new_name:str):
+        assert isinstance(new_name, str), "New name must be a string."
+        assert new_name is not None and (len(new_name) > 0), "New name must not be empty."
+        assert new_name != self.name, "New name must be different from the current name."
+        
+        self.name = new_name
+
+    def add_habit(self, habit:Habit):
+        assert habit.name not in [h.name for h in self.habits], "Habit name already exists in the group."
+        assert habit is not None, "Added object must not be empty."
+        assert len(habit.name) > 0, "Added object must have a valid name."
+        assert habit.type == self.name, "Habit type must match the group name."
+
+        self.habits.append(habit)
+
+    def remove_habit(self, habit:Habit):
+        self.habits.remove(habit)
 
 def register_habits(habits_file):
     with open(habits_file, 'r') as hf:
         pass
+
+print("It is working.")
