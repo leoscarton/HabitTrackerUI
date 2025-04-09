8b89e94e6ef9f94e08b140f47b87538654790d6f
leoscarton <lfscarton@gmail.com>
2025-04-08 21:36:54 -0300

    Enhanced CSV file reading function

--- main.py
+++ main.py
@@ -8,9 +8,16 @@ from PySide6.QtWidgets import QApplication
 def register_habits(habits_file):
     with open(habits_file, 'r') as hf:
         df = pd.read_csv(hf, sep=';')
+        df.dropna(inplace=True, axis=0)
+
         df['Done'] = df['Done'].astype(bool)
+        return df
 
 if __name__ == "__main__":
+    # Register habits from a CSV file
+    habits_file = 'habits_df_file.csv'
+    df = register_habits(habits_file)
+
     app = QApplication(sys.argv)
     window = habits_gui.MainWindow()
     window.show()
